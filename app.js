b15c38bb1de1497d625e46e35fb7144078347c7b
Jake Thrasher <mjakethrasher@gmail.com>
2020-12-20 02:53:46 -0800

    update js to be more concise

---
+++ app.js
@@ -0,0 +1,28 @@
+const input1=document.getElementById('input-1');
+const input2=document.getElementById('input-2');
+const resultDisplay=document.getElementById('result-display');
+
+
+const buttonsArray=document.querySelectorAll('button');
+
+for(let button of buttonsArray){
+
+    button.addEventListener('click',()=>{
+        
+        let num1=Number(input1.value);
+        let num2=Number(input2.value);
+        let result;
+        
+        if(button.id==="add-button"){
+            result = num1 + num2;
+        }else if(button.id==="subtract-button"){
+            result= num1 - num2;
+        }else if(button.id==="multiply-button"){
+            result= num1 * num2;
+        }else if(button.id==="divide-button"){
+            result= num1 / num2;
+        }
+        resultDisplay.innerHTML = result;
+
+    })
+}
